bf02a3b2d81d35955a19dc887a83b81aa9b32d41
Izuchukwu Ogbodo <matthiasogbodo@gmail.com>
2019-05-20 18:24:40 +0100

    Admin can get a user by id

--- constructors/admin.js
+++ constructors/admin.js
@@ -11,3 +11,15 @@ function Admin(firstName, lastName, gender) {
 
 //Make Admin inherits from User
 inheritProperty(user, Admin);
+
+//Get a user by id
+Admin.prototype.getUserByID = function(id) {
+  var users = this.getUsers(); //Returns the collection of Users
+
+  for (var index = 0; index < users.length; index++) {
+    //Compare each user id with the user id we are interested in and return it.
+    if (users[index].id === id) return users[index];
+  }
+
+  return false; //Returns false if no user with such id exists
+};
